681957a9608853d2515afe52acc8e96bb1bd142e
Aliaksei Babich <lex.gmx@gmail.com>
2020-10-18 18:24:40 +0300

    init: implement a function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,53 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let exprArr = expr.match(/.{1,10}/g);
+	let morseArr = [];
+	let finalExpr = "";
+
+	exprArr.map((item) => {
+		morseArr.push(item.match(/.{1,2}/g));
+	});
+
+	const decodeToMorse = (morseArr) => {
+		for (let i = 0; i < morseArr.length; i++) {
+			for (let j = 0; j < morseArr.length; j++) {
+				if (morseArr[i][j] === "10") {
+					morseArr[i][j] = ".";
+				} else if (morseArr[i][j] === "11") {
+					morseArr[i][j] = "-";
+				} else if (morseArr[i][j] === "00") {
+					morseArr[i].splice([j], 1);
+					decodeToMorse(morseArr);
+				}
+			}
+		}
+		return morseArr;
+	};
+	
+	decodeToMorse(morseArr);
+
+	const finalArr = morseArr.map(elem => {
+		return elem.join("");
+	});
+
+	console.log(finalArr);
+
+	for (let i = 0; i < finalArr.length; i++) {
+		for (let key in MORSE_TABLE) {
+			if (key === finalArr[i]) {
+				finalExpr += MORSE_TABLE[key];
+			} else if (finalArr[i] === '**********') {
+				finalExpr += ' ';
+			}
+		}
+	}
+
+	finalExpr = finalExpr.replace(/ +/g, ' ').trim();
+
+	return finalExpr;
 }
 
 module.exports = {
     decode
-}
+};
